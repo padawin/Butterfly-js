03e9ecfdfae0d6886a0bff20c2e577ad68dc1204
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-08-28 13:36:39 +0100

    AJAX code cleaned, refactored and commented

--- js/Butterfly/Ajax.js
+++ js/Butterfly/Ajax.js
@@ -1,6 +1,6 @@
 (function(){
-	window.B || throw 'Butterfly base library is needed';
-	var B.Ajax={};
+	if (!window.B) throw 'Butterfly base library is needed';
+	window.B.Ajax={};
 
 	/**
 	 *
@@ -32,23 +32,30 @@
 				valueEnd=s.match(scriptEndRegexp);
 				//if a script is found, set pos2 at the script position
 				pos2=(~posEnd?posEnd:s.length-1);
-				scripts.push(s.subsing(pos1+valueStart[0].length,pos2));
+				scripts.push(s.substring(pos1+valueStart[0].length,pos2));
 				val2=valueEnd[0].length;
 			}
-			s=s.subs(pos2+val2,s.length-1);
+			s=s.substring(pos2+val2,s.length-1);
 		}
 		return scripts;
 	}
 
 	/**
+	 * Method to execute AJAX requests.
 	 *
-	 * behaviors : functions for what to do on each status ans readyState
+	 * behaviors: functions for what to do on each status and readyState.
+	 * 		Associative array, with as keys the status and readyState values.
+	 * 		For example, to execute a function when the status is 200,
+	 * 		behaviorsStatus must be {200: function(xhr){//do things here}}
+	 * options: associative array. For the moment, there is only one possible
+	 * 		value :
+	 * 		- async: if true, the request will be executed in asynchronous mode.
 	 *
 	 */
 	B.Ajax.request=function(url,behaviorsStatus,behaviorsReadyState,method,params,options){
-		!behaviorsStatus && behaviorsStatus={};
-		!behaviorsReadyState && behaviorsReadyState={};
-		!options && options={};
+		behaviorsStatus = behaviorsStatus || {};
+		behaviorsReadyState = behaviorsReadyState || {};
+		options = options || {};
 
 		var xhr;
 		if(window.XMLHttpRequest) xhr = new XMLHttpRequest();
@@ -76,154 +83,19 @@
 		}
 	};
 
-
+	/**
+	 * Shortcut method of request to update a node with the request response.
+	 */
 	B.Ajax.update=function(nodeToUpdate,url,evalScripts,method,params,options){
 		var updateFunc=function(xhr){
-			$id(nodeToUpdate).innerHTML = xhr.responseText;
-			// WHY timeout???
-			setTimeout(function(){
+			B.$id(nodeToUpdate).innerHTML=xhr.responseText;
 				if(evalScripts){
-					var scripts=getScripts(xhr.responseText);
-					var nbScripts=scripts.length;
-					for(var i in scripts) window.eval(scripts[i]);
-				}
-			},700);
+				var scripts=getScripts(xhr.responseText);
+				for(var i in scripts) window.eval(scripts[i]);
+			}
 		};
 
 		B.Ajax.request(url,{200:updateFunc},{},method,params,options);
 	};
 
 })();
-/**
- *
- * Class to do ajax requests with Butterfly
- *
- */
-
-
-Butterfly.Ajax = {
-
-	/**
-	 *
-	 * behaviors : functions for what to do on each status ans readyState
-	 *
-	 */
-	request: function(url, behaviorsStatus, behaviorsReadyState, method, params, options)
-	{
-
-		if (behaviorsStatus == null) {
-			behaviorsStatus = {};
-		}
-
-		if (behaviorsReadyState == null) {
-			behaviorsReadyState = {};
-		}
-
-		if (options == null) {
-			options = {};
-		}
-
-		var xhr;
-		if (window.XMLHttpRequest) {
-			xhr = new XMLHttpRequest();
-		}
-		else if (window.ActiveXObject) {
-			xhr = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-
-		if (xhr) {
-			xhr.onreadystatechange = function()
-			{
-				if(xhr.readyState  == 4) {
-					if(behaviorsStatus[xhr.status]) {
-						var func = behaviorsStatus[xhr.status];
-						func(xhr);
-					}
-				}
-				else {
-					if (behaviorsReadyState[xhr.readyState]) {
-						var func = behaviorsReadyState[xhr.readyState];
-						func(xhr);
-					}
-				}
-			};
-
-			xhr.open( method == 'POST' ? method : 'GET', url,  (Butterfly.exists(options.async) ? options.async : true));
-
-			if (method == 'POST') {
-				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xhr.setRequestHeader("Content-length", params.length);
-				xhr.setRequestHeader("Connection", "close");
-			}
-
-			xhr.send( method == 'POST' ? params : null );
-
-			return xhr;
-		}
-	},
-
-	/**
-	 *
-	 * extract all js code in <scripts> tags and return an array
-	 * containing the codes
-	 *
-	 * For now, htmls is useless... maybe for another use ?
-	 * str is [html part][script part][html part][script part]...
-	 *
-	 * [html part]s go in htmls var
-	 * [script part]s go in scripts var
-	 *
-	 */
-	_getScriptsFromString: function(str)
-	{
-		var scriptBeginRegexp = /<script[\stype="'x\/javascri]*>/gi;
-		var scriptEndRegexp = /<\/[\s]*script>/gi;
-
-		var posFirst, valueFirst;
-		var posSecond, valueSecond;
-		var pos1, pos2, val2;
-		var htmls = [];
-		var scripts = [];
-
-		while (str != '') {
-			//return the position of the first script open tag (<script>)
-			posFirst = str.search(scriptBeginRegexp);
-			valueFirst = str.match(scriptBeginRegexp);
-
-			//if a script is found
-			if (posFirst != -1) {
-				pos1 = posFirst;
-			}
-			else {
-				pos1 = str.length - 1;
-			}
-
-			//put the current html part
-			htmls.push(str.substring(0, pos1));
-
-			if (posFirst != -1) {
-				posSecond = str.search(scriptEndRegexp);
-				valueSecond = str.match(scriptEndRegexp);
-
-				if (posSecond != -1) {
-					pos2 = posSecond;
-				}
-				else {
-					pos2 = str.length - 1;
-				}
-				scripts.push(str.substring(pos1 + valueFirst[0].length, pos2));
-				val2 = valueSecond[0].length;
-			}
-			else {
-				//stop the loop
-				pos2 = str.length - 1;
-				val2 = 0;
-			}
-
-			str = str.substr(pos2 + val2, str.length - 1);
-		}
-
-		return scripts;
-	}
-
-};
