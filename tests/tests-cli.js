de47d7fa2d0c69f6f4fdd7df189ace2597c3b584
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-27 14:26:55 +0000

    dependancies

--- tests/tests-cli.js
+++ tests/tests-cli.js
@@ -2,6 +2,7 @@
 var tests = require('./tests.js').Tests;
 require('./tests-of-tests.js');
 require('../src/js/loader.js');
+require('../src/js/ajax.js');
 require('../src/js/template.js');
 require('./template.js');
 
